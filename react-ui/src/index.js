fe7ea13cc952aff5e6cb07268d5652938a10aa10
Ian Harvey <ith.harvey@gmail.com>
2017-09-14 16:45:45 -0500

    changed react front end connection

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import PeopleBox from './components/PeopleBox';
 
 ReactDOM.render(
-   <PeopleBox url='https://profile-crud-app.herokuapp.com/api/people' pollInterval={2000} />,
+   <PeopleBox url='https://profile-crud-react-routes.herokuapp.com/api/people' pollInterval={2000} />,
  document.getElementById('root')
 );
